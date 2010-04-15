f9412fd856df2a199406b60fec7cdabd21cafa01
Mark Choate <mchoate@choate-t11e.local>
2010-04-15 16:44:29 -0400

    Refactored breadcrumb widget to work with both jQuery UI 1.7.2 and 1.8.

--- src/web/widgets/js/t11e/widget/jquery/breadcrumb_widget.js
+++ src/web/widgets/js/t11e/widget/jquery/breadcrumb_widget.js
@@ -4,100 +4,63 @@
  */
 
 (function ($) {
-    $.widget('ui.t11e_breadcrumb', {});
-
-    $.ui.t11e_breadcrumb.defaults = {
-        search_group: 'default',
-        page_param: 'page',
-        value_params: [],
-        close_template: '&nbsp;[<a class="t11e-close" href="#">x</a>]',
-        container: '.t11e-widget-jquery-breadcrumb-bd',
-        animate: false,
-        animation_speed: 'fast'
-    };
+    var options = {
+            search_group: 'default',
+            page_param: 'page',
+            value_params: [],
+            close_template: '&nbsp;[<a class="t11e-close" href="#">x</a>]',
+            container: '.t11e-widget-jquery-breadcrumb-bd',
+            animate: false,
+            animation_speed: 'fast'
+        };
+    $.widget('ui.t11e_breadcrumb', { options: options });
+    $.ui.t11e_breadcrumb.defaults = options;
 
     /*jslint nomen: false */
     $.ui.t11e_breadcrumb.prototype._init = function () {
         var self = this;
 
-        var add_breadcrumb = function (param, value) {
-            var breadcrumb = self.element.find(self.options.container);
-            var crumb = self._get_breadcrumb(param, value);
-            if (crumb.size() === 0) {
-                crumb = $('<div class="t11e-breadcrumb ui-state-default ui-corner-all" param="' +
-                    param + '" value="' + value + '">' + value + self.options.close_template + '</div> ');
-                crumb.hover(
-                    function () {
-                        $(this).addClass("ui-state-hover");
-                    },
-                    function () {
-                        $(this).removeClass("ui-state-hover");
-                    }
-                );
-                if (self.options.animate) {
-                    crumb.hide().appendTo(breadcrumb).fadeIn(self.options.animation_speed);
-                } else {
-                    crumb.appendTo(breadcrumb);
-                }
+        t11e.event.subscribe('request.' + self.options.search_group, function (params) {
+            self._load_params(params);
+        });
 
-                var crumb_anchor = crumb.find('a:first');
-                crumb_anchor.bind('click', function () {
-                    var crumb_param = crumb.attr('param');
-                    var crumb_value = crumb.attr('value');
-                    t11e.event.trigger('update_request.' + self.options.search_group, function (params) {
-                        remove_breadcrumb(params, crumb_param, crumb_value);
-                        t11e.util.remove_param(params, self.options.page_param);
-                    });
-                    return false;
-                });
-            }
-        };
+        t11e.event.subscribe('clear_params_from_search.' + self.options.search_group, function (params) {
+            self._clear_params(params);
+        });
+    };
 
-        var remove_breadcrumb = function (params, param, value) {
-            var crumb = self._get_breadcrumb(param, value);
-            if (self.options.animate) {
-                crumb.fadeOut(self.options.animation_speed, function () {
-                    $(this).remove();
-                });
-            } else {
-                crumb.remove();
-            }
-            t11e.util.remove_param_value(params, param, value);
-        };
-
-        var load_from_params = function (params) {
-            if (t11e.util.is_defined(self.options.value_params)) {
-                $.each(self.options.value_params, function (i, param) {
-                    var crumbs = self._get_breadcrumbs(param);
-                    values = params[param];
-                    if (t11e.util.is_undefined(values)) {
-                        values = [];
-                    }
-                    crumbs.each(function (i, crumb) {
-                        var crumb_param = $(crumb).attr('param');
-                        var crumb_value = $(crumb).attr('value');
-                        var remove = (-1 === $.inArray(crumb_value, values));
-                        if (remove) {
-                            remove_breadcrumb(params, crumb_param, crumb_value);
-                        }
-                    });
-                    $.each(values, function (i, value) {
-                        add_breadcrumb(param, value);
-                    });
-                });
-            }
-        };
-        t11e.event.subscribe('request.' + self.options.search_group, load_from_params);
-
-        var clear_params_from_search = function (params) {
+    $.ui.t11e_breadcrumb.prototype._load_params = function (params) {
+        var self = this;
+        if (t11e.util.is_defined(self.options.value_params)) {
             $.each(self.options.value_params, function (i, param) {
-                var values = self._get_param_values(param);
+                var crumbs = self._get_breadcrumbs(param);
+                values = params[param];
+                if (t11e.util.is_undefined(values)) {
+                    values = [];
+                }
+                crumbs.each(function (i, crumb) {
+                    var crumb_param = $(crumb).attr('param');
+                    var crumb_value = $(crumb).attr('value');
+                    var remove = (-1 === $.inArray(crumb_value, values));
+                    if (remove) {
+                        self.remove_breadcrumb(params, crumb_param, crumb_value);
+                    }
+                });
                 $.each(values, function (i, value) {
-                    remove_breadcrumb(params, param, value);
+                    self.add_breadcrumb(param, value);
                 });
             });
-        };
-        t11e.event.subscribe('clear_params_from_search.' + self.options.search_group, clear_params_from_search);
+        }
+    };
+
+    $.ui.t11e_breadcrumb.prototype._clear_params = function (params) {
+        var self = this;
+        $.each(self.options.value_params, function (i, param) {
+            var values = self._get_param_values(param);
+            $.each(values, function (i, value) {
+                self.remove_breadcrumb(params, param, value);
+            });
+        });
     };
 
     $.ui.t11e_breadcrumb.prototype._get_breadcrumb = function (param, value) {
@@ -109,11 +72,58 @@
     };
 
     $.ui.t11e_breadcrumb.prototype._get_param_values = function (param) {
-        var crumbs = this.find('div[param=' + param + ']');
+        var crumbs = this.element.find('div[param=' + param + ']');
         var results =  $.map(crumbs, function (item, i) {
             return $(item).attr('value');
         });
         return results;
+    };
+
+    $.ui.t11e_breadcrumb.prototype.add_breadcrumb = function (param, value) {
+        var self = this;
+        var breadcrumb = self.element.find(self.options.container);
+        var crumb = self._get_breadcrumb(param, value);
+        if (crumb.size() === 0) {
+            crumb = $('<div class="t11e-breadcrumb ui-state-default ui-corner-all" param="' +
+                param + '" value="' + value + '">' + value + self.options.close_template + '</div> ');
+            crumb.hover(
+                function () {
+                    $(this).addClass("ui-state-hover");
+                },
+                function () {
+                    $(this).removeClass("ui-state-hover");
+                }
+            );
+            if (self.options.animate) {
+                crumb.hide().appendTo(breadcrumb).fadeIn(self.options.animation_speed);
+            } else {
+                crumb.appendTo(breadcrumb);
+            }
+
+            var crumb_anchor = crumb.find('a:first');
+            crumb_anchor.bind('click', function () {
+                var crumb_param = crumb.attr('param');
+                var crumb_value = crumb.attr('value');
+                t11e.event.trigger('update_request.' + self.options.search_group, function (params) {
+                    self.remove_breadcrumb(params, crumb_param, crumb_value);
+                    t11e.util.remove_param(params, self.options.page_param);
+                });
+                return false;
+            });
+        }
+    };
+
+    $.ui.t11e_breadcrumb.prototype.remove_breadcrumb = function (params, param, value) {
+        var self = this;
+        var crumb = self._get_breadcrumb(param, value);
+        if (self.options.animate) {
+            crumb.fadeOut(self.options.animation_speed, function () {
+                $(self).remove();
+            });
+        } else {
+            crumb.remove();
+        }
+        t11e.util.remove_param_value(params, param, value);
     };
 
     $.ui.t11e_breadcrumb.prototype.destroy = function () {
