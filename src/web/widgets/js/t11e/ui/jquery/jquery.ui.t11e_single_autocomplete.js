b5e496f22d6ed8b5897f3d3e08f3eaf62a9aee93
Terry Smith <terry@t11e.com>
2010-07-15 09:24:07 -0400

    Added t11e_single_autocomplete widget.
    http://transparensee.jira.com/browse/WIDGETS-8

---
+++ src/web/widgets/js/t11e/ui/jquery/jquery.ui.t11e_single_autocomplete.js
@@ -0,0 +1,52 @@
+/**
+ * @copyright Transparensee Systems, Inc.
+ * @fileOverview $.t11e_single_autocomplete definition
+ */
+
+/**
+ * Autocomplete search widget.
+ *
+ * <h2>Example</h2>
+ * <div class="t11e-widget-example"><!--
+ *   <div id="example" class="t11e-widget t11e-widget-jquery-single-autocomplete">
+ *    <div class="t11e-hd t11e-widget-jquery-single-autocomplete-hd"></div>
+ *    <div class="t11e-bd t11e-widget-jquery-single-autocomplete-bd">
+ *      <form action="" onsubmit="return false;">
+ *        <input name="k">
+ *      </form>
+ *    </div>
+ *    <div class="t11e-ft t11e-widget-jquery-single-autocomplete-ft"></div>
+ *   </div>
+ *   <script type="text/javascript">
+ *     $('#example').t11e_single_autocomplete({
+ *       'search_group': 'default',
+ *       'value_param': 'myparam',
+ *       'source': '/autocomplete',
+ *       'source_params': {'listlength': 3}
+ *     });
+ *   </script>
+ * --></div>
+ * @name jQuery.ui.t11e_single_autocomplete
+ * @class A widget for single select autocomplete.
+ */
+(function ($) {
+    $.widget('ui.t11e_single_autocomplete', {});
+    $.ui.t11e_single_autocomplete.defaults = {
+    };
+
+    /*jslint nomen: false */
+    $.ui.t11e_single_autocomplete.prototype._init = function () {
+        var self = this;
+        var autocomplete = $(self.element).find('input:first');
+        autocomplete.t11e_autocomplete(self.options);
+        autocomplete.bind('autocompleteselect', function (event, ui) {
+            autocomplete.change();
+        });
+        $(self.element).t11e_textbox(self.options);
+    };
+
+    $.ui.t11e_single_autocomplete.prototype.destroy = function () {
+        this.element.unbind();
+        $.widget.prototype.destroy.apply(this, arguments);
+    };
+}(jQuery));
